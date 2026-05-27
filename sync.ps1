b27496e412a9854c82b44868456d69a94ef46ecc
RayHentai <zhangjunyi12345678@gmail.com>
2026-05-27 22:52:19 +0800

    update notes 2026-05-27 22:52

---
+++ sync.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "syncing..." -ForegroundColor Cyan
+Remove-Item -Path "C:\Users\Administrator\quartz\content\*" -Recurse -Force
+Copy-Item -Path "D:\OneDrive\文档\Obsidian\Hentai的知识库\*" -Destination "C:\Users\Administrator\quartz\content" -Recurse -Force
+Write-Host "pushing to GitHub..." -ForegroundColor Cyan
+Set-Location "C:\Users\Administrator\quartz"
+git add .
+git commit -m "update notes $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
+git push
+Write-Host "done!" -ForegroundColor Green
